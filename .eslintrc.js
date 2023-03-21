652eefa426be93f4c83b0da8988734f3d2b75192
kriptonian <shymal.sawan@gmail.com>
2023-03-22 00:39:17 +0530

    feat: basic setup

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	root: true,
+	extends: '@react-native-community',
+	rules: {
+		indent: ['error', 'tab', {SwitchCase: 1}],
+		// quotes: ['error', 'double'],
+		// 'object-curly-spacing': ['error', 'always'],
+		// 'array-bracket-spacing': ['error', 'always'],
+	},
+};
