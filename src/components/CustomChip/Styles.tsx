440329d845968e5eee5c24e4a31da4de2c47b2b6
kriptonian <shymal.sawan@gmail.com>
2023-03-23 19:43:54 +0530

    fix: conflict part2

--- src/components/CustomChip/Styles.tsx
+++ src/components/CustomChip/Styles.tsx
@@ -7,14 +7,10 @@ export default StyleSheet.create({
 		height: 48,
 		alignItems: 'center',
 		justifyContent: 'center',
-<<<<<<<< HEAD:src/components/CustomChip/Styles.tsx
 		borderWidth: 1.5,
 		gap: 4,
 		borderRadius: 10,
-========
 		paddingVertical: 10,
-		borderRadius: 16,
->>>>>>>> remotes/origin/dev:src/components/PrimaryButton/Styles.tsx
 	},
 	text: {
 		fontSize: 15,
