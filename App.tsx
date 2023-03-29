e9e74813e5962e09edb4eb74cd2aeaf1cb203896
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2023-03-30 03:06:44 +0530

    feat: refer friend

--- App.tsx
+++ App.tsx
@@ -9,46 +9,45 @@
  * @format
  */
 
- import React, { useEffect } from "react";
- import { SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
- import { NavigationContainer } from "@react-navigation/native";
- import { createNativeStackNavigator } from "@react-navigation/native-stack";
- import SplashScreen from "react-native-splash-screen";
- 
- // import AppStyles from "@/AppStyles";
- import { AppNavigation } from "@/navigations";
- import { PaymentHistory, PaymentProcessScreen } from "./src/screens";
- 
- const Stack = createNativeStackNavigator();
- 
- function App(): JSX.Element {
-	 useEffect(() => {
-		 SplashScreen.hide();
-	 });
- 
-	 return (
-		 <View style={styles.container}>
-			 <StatusBar
-				 translucent
-				 barStyle="default"
-				 backgroundColor="transparent"
-			 />
-			 <NavigationContainer>
-				 <AppNavigation />
-				 {/*<PaymentProcessScreen />*/}
-				 {/* <PaymentHistory /> */}
-			 </NavigationContainer>
-		 </View>
-	 );
- }
- 
- const styles = StyleSheet.create({
-	 container: {
-		 flex: 1,
-		 flexGrow: 1,
-		 backgroundColor: "#FFFFFF",
-	 },
- });
- 
- export default App;
- 
+import React, { useEffect } from "react";
+import { SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SplashScreen from "react-native-splash-screen";
+
+// import AppStyles from "@/AppStyles";
+import { AppNavigation } from "@/navigations";
+import { PaymentHistory, PaymentProcessScreen } from "./src/screens";
+
+const Stack = createNativeStackNavigator();
+
+function App(): JSX.Element {
+	useEffect(() => {
+		SplashScreen.hide();
+	});
+
+	return (
+		<View style={styles.container}>
+			<StatusBar
+				translucent
+				barStyle="default"
+				backgroundColor="transparent"
+			/>
+			<NavigationContainer>
+				<AppNavigation />
+				{/*<PaymentProcessScreen />*/}
+				{/* <PaymentHistory /> */}
+			</NavigationContainer>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexGrow: 1,
+		backgroundColor: "#FFFFFF",
+	},
+});
+
+export default App;
