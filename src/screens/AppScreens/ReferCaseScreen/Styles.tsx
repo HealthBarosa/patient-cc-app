5a56632fa96dd8f6ee641767e17672972112b3bc
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2023-05-03 23:13:36 +0530

    feat: share

--- src/screens/AppScreens/ReferCaseScreen/Styles.tsx
+++ src/screens/AppScreens/ReferCaseScreen/Styles.tsx
@@ -1,5 +1,14 @@
+import AppStyles from "@/AppStyles";
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-	container: {},
+	container: {
+		flex: 1,
+		backgroundColor: AppStyles.colorWhite,
+	},
+	wrapper: {
+		flex: 1,
+		paddingHorizontal: 20,
+		paddingTop: 20,
+	},
 });
